cbe883381a7ac0b5acb15c0ed40827575dd98910
justmike1 <90835468+justmike1@users.noreply.github.com>
2022-02-17 05:10:55 +0200

    date filtration per exchange + more robust fetch_klines func

--- average_price_calc.py
+++ average_price_calc.py
@@ -20,97 +20,89 @@ class AveragePrice:
         self.market = str(data['market'])  # every exchange utilizes different format
         self.start_date = data['start_date']
         self.end_date = data['end_date']
-        # must be in that format
-        self.ts_start = int(datetime.strptime(self.start_date, "%m/%d/%Y %H").timestamp())
-        self.ts_end = int(datetime.strptime(self.end_date, "%m/%d/%Y %H").timestamp())
-        # ISO 8601 conversion
-        self.ts_start_iso = datetime.fromtimestamp(self.ts_start).isoformat()
-        self.ts_end_iso = datetime.fromtimestamp(self.ts_end).isoformat()
 
-        if self.exchange in ('binance', 'ascendex', 'bitfinex'):
-            self.ts_start *= 1000
-            self.ts_end *= 1000
-        elif 'coinbase' == self.exchange:
-            self.ts_start = self.ts_start_iso
-            self.ts_end = self.ts_end_iso
+        def calc_date_for_exchange(date, exchange):
+            # must be in that format
+            ts = int(datetime.strptime(date, "%m/%d/%Y %H").timestamp())
+            if exchange in ('binance', 'ascendex', 'bitfinex'):
+                ts *= 1000
+            elif exchange == 'coinbase':
+                # ISO 8601 conversion
+                ts = datetime.fromtimestamp(ts).isoformat()
+            return ts
+
+        self.ts_start = calc_date_for_exchange(self.start_date, self.exchange)
+        self.ts_end = calc_date_for_exchange(self.end_date, self.exchange)
 
         self.average = 0
         self.price_database = []
 
-        self.start()
-
-    def fetch_klines(self, market, start, end):
-        for exchange, val in {
+        self.price_endpoints = {
             'binance': [
-                f"https://api.binance.com/api/v3/klines?symbol={market}&interval=1h&startTime={start}&endTime={end}",
+                f"https://api.binance.com/api/v3/klines?symbol={self.market}&interval=1h&startTime={self.ts_start}&endTime={self.ts_end}",
                 lambda res_data: res_data, 1],
             'coinbase': [
-                f"https://api.exchange.coinbase.com/products/{market}/candles?start={start}&end={end}&granularity=3600",
+                f"https://api.exchange.coinbase.com/products/{self.market}/candles?start={self.ts_start}&end={self.ts_end}&granularity=3600",
                 lambda res_data: res_data, 3],
             'gateio': [
-                f"https://api.gateio.ws/api/v4/spot/candlesticks?currency_pair={market}&interval=1h&from={start}&to={end}",
+                f"https://api.gateio.ws/api/v4/spot/candlesticks?currency_pair={self.market}&interval=1h&from={self.ts_start}&to={self.ts_end}",
                 lambda res_data: res_data, -1],
             'kucoin': [
-                f"https://api.kucoin.com/api/v1/market/candles?type=1hour&symbol={market}&startAt={start}&endAt={end}",
+                f"https://api.kucoin.com/api/v1/market/candles?type=1hour&symbol={self.market}&startAt={self.ts_start}&endAt={self.ts_end}",
                 lambda res_data: res_data['data'], 1],
             'ascendex': [
-                f"https://ascendex.com/api/pro/v1/barhist?symbol={market}&interval=60&from={start}&to={end}",
+                f"https://ascendex.com/api/pro/v1/barhist?symbol={self.market}&interval=60&from={self.ts_start}&to={self.ts_end}",
                 lambda res_data: res_data['data'], None],
             'bitfinex': [
-                f"https://api-pub.bitfinex.com/v2/candles/trade:1h:{market}/hist?start={start}&end={end}",
+                f"https://api-pub.bitfinex.com/v2/candles/trade:1h:{self.market}/hist?start={self.ts_start}&end={self.ts_end}",
                 lambda res_data: res_data, 1],
             'ftx': [
-                f"https://ftx.com/api/markets/{market}/candles?resolution=3600&start_time={start}&end_time={end}",
+                f"https://ftx.com/api/markets/{self.market}/candles?resolution=3600&start_time={self.ts_start}&end_time={self.ts_end}",
                 lambda res_data: res_data['result'], None],
-        }.items():
-            url = val[0]
-            fetch = val[1]
-            index = val[2]
-            if exchange == self.exchange:
-                res = rq.get(url)
-                if res.status_code != 200:
-                    logging.error(f"{res.status_code}, check {exchange} and {market} in the config")
-                    return
-                else:
-                    logging.info(f"successfully fetched market {market} in {exchange}")
-                    return self.get_price(exchange, fetch(res.json()), index)
+        }
 
+        self.start()
+
+    def fetch_klines(self):
+        vals = self.price_endpoints[self.exchange]
+        url, fetch, index, *_ = vals
+        res = rq.get(url)
+        if res.status_code != 200:
+            logging.error(f"{res.status_code}, check {self.exchange} and {self.market} in the config")
+        else:
+            logging.info(f"successfully fetched market {self.market} in {self.exchange}")
+            return self.get_price(self.exchange, fetch(res.json()), index)
 
     def get_price(self, exchange, open_candle, index):
         for price in open_candle:
             if exchange == 'ascendex':
                 price = float(dict(price).get('data', {}).get('o'))
-                logging.info(f"{price} {len(self.price_database)}")
-                self.price_database.append(price)
             elif exchange == 'ftx':
                 price = float(dict(price).get('open'))
-                logging.info(f"{price} {len(self.price_database)}")
-                self.price_database.append(price)
             else:
                 price = float(price[index])
-                logging.info(f"{price} {len(self.price_database)}")
-                self.price_database.append(price)
-        return
+
+            logging.info(f"{price} {len(self.price_database)}")
+            self.price_database.append(price)
 
     def get_average(self):
-        try:
-            self.average = sum(self.price_database) / len(self.price_database)
-            if self.average > 1:
-                self.average = round(sum(self.price_database) / len(self.price_database), 5)
-            logging.info(f"the amount of prices saved in the database is {len(self.price_database)} (= hours searched)")
-            logging.info(f"the average price of the whole performance is {self.average}")
-            return self.average
-        except ZeroDivisionError as zde:
+        if len(self.price_database) == 0:
             if self.exchange == 'coinbase':
                 logging.error("Check the date in config, coinbase only allows 300 hours max (12.5 days)")
             else:
-                logging.error(f"The API didn't fetch any data: {zde}, check config")
+                logging.error(f"The API didn't fetch any data, check config, if persists contact Mike")
+        else:
+            self.average = sum(self.price_database) / len(self.price_database)
+            if self.average > 1:
+                self.average = round(self.average, 5)
+            logging.info(f"the amount of prices saved in the database is {len(self.price_database)} (= hours searched)")
+            logging.info(f"the average price of the whole performance is {self.average}")
 
     def start(self):
-        if re.search('[a-zA-Z]', self.start_date + self.end_date):
-            raise Exception("date config error")
+        if self.exchange not in self.price_endpoints.keys() or re.search('[a-zA-Z]', self.start_date + self.end_date):
+            raise Exception("You have a typo in config or exchange is not supported, contact Mike")
         logging.info(f"start timestamp: {self.ts_start}, end timestamp: {self.ts_end}")
-        self.fetch_klines(self.market, self.ts_start, self.ts_end)
+        self.fetch_klines()
         self.get_average()
 
 
